d62db29bd24ef3b43241a3ebe9e4fe435caec3b9
David Dossot <david@dossot.net>
2013-09-27 18:51:15 -0700

    updated code for ch 5, 6 and 9

--- chapter06/src/main/resources/md5.component.js
+++ chapter06/src/main/resources/md5.component.js
@@ -2,11 +2,11 @@ importPackage(org.apache.commons.codec.digest);
 
 var PROPERTY_NAME = "MD5SUM";
 
-if (!message.getProperty(PROPERTY_NAME)) {
+if (!message.getProperty(PROPERTY_NAME)) {//<co id="lis_06_rhino_md5_1"/>
     log.debug("Setting " + PROPERTY_NAME 
-    		+ " property for message: " + id);
-    message.setProperty(PROPERTY_NAME, getMD5Sum(payload));
-    result = message;
+    		+ " property for message: " + id);//<co id="lis_06_rhino_md5_2"/>
+    message.setProperty(PROPERTY_NAME, getMD5Sum(payload));//<co id="lis_06_rhino_md5_3"/>
+    result = message;//<co id="lis_06_rhino_md5_4"/>
 }
 
 function getMD5Sum(input) {
